a6583dc5a748f65196754fb3b99c7617f06c2882
AbinashDwibedi <dwibedi.abinash123@gmail.com>
2026-01-20 09:05:46 +0530

    some basic recursion questions solutions added

---
+++ Recursion/basic.py
@@ -0,0 +1,26 @@
+# print name 5 times
+# def name(i,n):
+#     if i == n:
+#         return 
+#     print("Abinash")
+#     i +=1 
+#     name(i,n)
+# name(0,5)
+
+# print linearly from 1 to n
+# def printNum(i,n):
+#     if i == n+1:
+#         return
+#     print(i)
+#     i+=1
+#     printNum(i,n)
+# printNum(1,50)
+
+# print from 1 to n usng backtracking 
+# def backtrack(i):
+#     if i == 0:
+#         return 
+#     i-=1
+#     backtrack(i)
+#     print(i)
+# backtrack(100)
