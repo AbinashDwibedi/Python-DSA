610e5e5bf5f9ccf7b6798d7eeec0299b052408d1
AbinashDwibedi <dwibedi.abinash123@gmail.com>
2026-03-08 16:45:09 +0530

    find different binary string java solution added

---
+++ findDifferentBinaryString.java
@@ -0,0 +1,37 @@
+// class Solution {
+//     public String findDifferentBinaryString(String[] nums) {
+//         int n = nums.length;
+//         int size = (int) Math.pow(2, n);
+//         int[] arr = new int[size];
+
+//         for (String num : nums) {
+//             int tmp = Integer.parseInt(num, 2);
+//             arr[tmp]++;
+//         }
+
+//         for (int i = 0; i < size; i++) {
+//             if (arr[i] == 0) {
+//                 String ans = Integer.toBinaryString(i);
+//                 return "0".repeat(n - ans.length()) + ans;
+//             }
+//         }
+
+//         return "0".repeat(n);
+//     }
+// }
+
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        String ans = new String("");
+        for(int i = 0;i<n;i++){
+            if(nums[i].charAt(i) == '0'){
+                ans += "1";
+            }
+            else{
+                ans += "0";
+            }
+        }
+        return ans;
+    }
+}
