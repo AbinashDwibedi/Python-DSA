7a4dff00aa7e32edd45ca63de4a2cdae6c05c928
AbinashDwibedi <dwibedi.abinash123@gmail.com>
2026-03-16 02:12:09 +0530

    some dsa problems solved

---
+++ Stack and queues/asteroidCollision.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+
+        for(int a : asteroids){
+            boolean destroyed = false;
+
+            while(!st.isEmpty() && st.peek() > 0 && a < 0){
+                if(Math.abs(st.peek()) < Math.abs(a)){
+                    st.pop();
+                    continue;
+                }
+                else if(Math.abs(st.peek()) == Math.abs(a)){
+                    st.pop();
+                }
+                destroyed = true;
+                break;
+            }
+
+            if(!destroyed){
+                st.push(a);
+            }
+        }
+
+        int[] result = new int[st.size()];
+        for(int i = result.length - 1; i >= 0; i--){
+            result[i] = st.pop();
+        }
+
+        return result;
+    }
+}
